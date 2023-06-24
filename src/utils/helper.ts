fa816ebe899d9c887543ca628771ab41951f11ae
mustafa batar <mustafa.batar@missafir.com>
2023-06-24 12:00:18 +0300

    feat(MWSV-39): Added getDummyDataByType in helpers and config changes tailwind and next.

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,6 +1,6 @@
 import { find, get } from "lodash";
 import { getMenu, getPage } from "@/service";
-import { PAGE_HEADER } from "@/components/molecules/header/constants";
+import { ILink } from "@/components/molecules/menu/types";
 
 interface IMenu {
   parent: {
@@ -24,4 +24,322 @@ export const getPageDataByComponent: any = async (
   return find(get(attributes, "home"), {
     __component: component
   });
+};
+
+export const getDummyDataByType = (type: string) => {
+  const dummyItems = {
+    dummyMenuItems: [
+      {
+        id: 1,
+        attributes: {
+          order: 1,
+          title: "Solutions",
+          url: "#",
+          target: "_blank",
+          createdAt: "created0",
+          updatedAt: "updated0",
+          children: {
+            data: [
+              {
+                id: 12,
+                attributes: {
+                  order: 1,
+                  title: "For Guests",
+                  url: "#",
+                  target: "_blank",
+                  createdAt: "created0",
+                  updatedAt: "updated0",
+                  children: {
+                    data: []
+                  }
+                }
+              },
+              {
+                id: 123,
+                attributes: {
+                  order: 1,
+                  title: "For Owners",
+                  url: "#",
+                  target: "_blank",
+                  createdAt: "created0",
+                  updatedAt: "updated0",
+                  children: {
+                    data: []
+                  }
+                }
+              },
+              {
+                id: 124,
+                attributes: {
+                  order: 1,
+                  title: "For Businesses",
+                  url: "#",
+                  target: "_blank",
+                  createdAt: "created0",
+                  updatedAt: "updated0",
+                  children: {
+                    data: []
+                  }
+                }
+              }
+            ]
+          }
+        }
+      },
+      {
+        id: 2,
+        attributes: {
+          order: 1,
+          title: "Services",
+          url: "#",
+          target: "_blank",
+          createdAt: "created0",
+          updatedAt: "updated0",
+          children: {
+            data: [
+              {
+                id: 12,
+                attributes: {
+                  order: 1,
+                  title: "Missafir Tech",
+                  url: "#",
+                  target: "_blank",
+                  createdAt: "created0",
+                  updatedAt: "updated0",
+                  children: {
+                    data: []
+                  }
+                }
+              },
+              {
+                id: 123,
+                attributes: {
+                  order: 1,
+                  title: "Missafir Design",
+                  url: "#",
+                  target: "_blank",
+                  createdAt: "created0",
+                  updatedAt: "updated0",
+                  children: {
+                    data: []
+                  }
+                }
+              },
+              {
+                id: 124,
+                attributes: {
+                  order: 1,
+                  title: "Advisor",
+                  url: "#",
+                  target: "_blank",
+                  createdAt: "created0",
+                  updatedAt: "updated0",
+                  children: {
+                    data: []
+                  }
+                }
+              },
+              {
+                id: 125,
+                attributes: {
+                  order: 1,
+                  title: "Greem",
+                  url: "#",
+                  target: "_blank",
+                  createdAt: "created0",
+                  updatedAt: "updated0",
+                  children: {
+                    data: []
+                  }
+                }
+              },
+              {
+                id: 126,
+                attributes: {
+                  order: 1,
+                  title: "Invite & Earn",
+                  url: "#",
+                  target: "_blank",
+                  createdAt: "created0",
+                  updatedAt: "updated0",
+                  children: {
+                    data: []
+                  }
+                }
+              }
+            ]
+          }
+        }
+      },
+      {
+        id: 3,
+        attributes: {
+          order: 1,
+          title: "Company",
+          url: "#",
+          target: "_blank",
+          createdAt: "created0",
+          updatedAt: "updated0",
+          children: {
+            data: [
+              {
+                id: 121,
+                attributes: {
+                  order: 1,
+                  title: "Meet the Team",
+                  url: "#",
+                  target: "_blank",
+                  createdAt: "created0",
+                  updatedAt: "updated0",
+                  children: {
+                    data: []
+                  }
+                }
+              },
+              {
+                id: 1202,
+                attributes: {
+                  order: 1,
+                  title: "Life at Missafir",
+                  url: "#",
+                  target: "_blank",
+                  createdAt: "created0",
+                  updatedAt: "updated0",
+                  children: {
+                    data: []
+                  }
+                }
+              },
+              {
+                id: 1242,
+                attributes: {
+                  order: 1,
+                  title: "Sustainability",
+                  url: "#",
+                  target: "_blank",
+                  createdAt: "created0",
+                  updatedAt: "updated0",
+                  children: {
+                    data: []
+                  }
+                }
+              },
+              {
+                id: 12429,
+                attributes: {
+                  order: 1,
+                  title: "Blog",
+                  url: "#",
+                  target: "_blank",
+                  createdAt: "created0",
+                  updatedAt: "updated0",
+                  children: {
+                    data: []
+                  }
+                }
+              }
+            ]
+          }
+        }
+      },
+      {
+        id: 4,
+        attributes: {
+          order: 1,
+          title: "Support",
+          url: "#",
+          target: "_blank",
+          createdAt: "created0",
+          updatedAt: "updated0",
+          children: {
+            data: [
+              {
+                id: 122229,
+                attributes: {
+                  order: 1,
+                  title: "Contact",
+                  url: "#",
+                  target: "_blank",
+                  createdAt: "created0",
+                  updatedAt: "updated0",
+                  children: {
+                    data: []
+                  }
+                }
+              },
+              {
+                id: 12355443,
+                attributes: {
+                  order: 1,
+                  title: "FAQ",
+                  url: "#",
+                  target: "_blank",
+                  createdAt: "created0",
+                  updatedAt: "updated0",
+                  children: {
+                    data: []
+                  }
+                }
+              }
+            ]
+          }
+        }
+      }
+    ],
+    dummyNavbarItems: {
+      logo: {
+        data: {
+          id: 7,
+          attributes: {
+            name: "logo-2.svg",
+            alternativeText: null,
+            caption: null,
+            width: 570,
+            height: 97,
+            formats: null,
+            hash: "logo_2_bfcd17cc2b",
+            ext: ".svg",
+            mime: "image/svg+xml",
+            size: 7.1,
+            url: "images/missafir-logo.svg",
+            previewUrl: null,
+            provider: "local",
+            provider_metadata: null,
+            createdAt: "2023-05-13T10:52:30.817Z",
+            updatedAt: "2023-05-22T19:29:24.954Z"
+          }
+        }
+      },
+      button: {
+        id: 32,
+        label: "Become a homeower ",
+        url: "https://homes.missafir.com/",
+        iconSrc: null
+      },
+      languages: [
+        {
+          id: 33,
+          label: "TR",
+          value: "tr",
+          url: null,
+          iconSrc: "/images/flags/turkish.svg"
+        },
+        {
+          id: 34,
+          label: "ME",
+          value: "me",
+          url: null,
+          iconSrc: "/images/flags/montenegrin.svg"
+        },
+        {
+          id: 35,
+          label: "EN",
+          value: "en",
+          url: null,
+          iconSrc: "/images/flags/english.svg"
+        }
+      ]
+    }
+  };
+  return get(dummyItems, `[${type}]`);
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import("tailwindcss").Config} */
-module.exports = {
+const plugin = require("tailwindcss/plugin");
+const config = {
   important: true,
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,10 +8,82 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
-    extend: {}
+    extend: {
+      fontSize: {
+        xxs: "0.63rem",
+        xxl: "1.38rem"
+      },
+      colors: {
+        primary: {
+          DEFAULT: "#B21A45",
+          50: "#FEC9EE",
+          100: "#F6D2DC",
+          200: "#F2A6BB",
+          300: "#EC799A",
+          400: "#E54D78",
+          500: "#D01E50",
+          600: "#B21A45",
+          700: "#861334",
+          800: "#590D22",
+          900: "#2D0611"
+        },
+        secondary: {
+          50: "#E8FAFC",
+          100: "#D2F5F9",
+          200: "#A4ECF4",
+          300: "#77E2EE",
+          400: "#49D9E9",
+          500: "#0E6973",
+          600: "#16A6B6",
+          700: "#117C88",
+          800: "#0B535B",
+          900: "#06292D"
+        },
+        darkblue: {
+          50: "#EEF2F7",
+          100: "#DCE5EF",
+          200: "#B9CBDF",
+          300: "#97B1CE",
+          400: "#3E878F",
+          500: "#0E6973",
+          600: "#41648B",
+          700: "#314B68",
+          800: "#203246",
+          900: "#101923"
+        },
+        grey: {
+          50: "#F2F2F2",
+          100: "#E6E6E6",
+          200: "#CCCCCC",
+          300: "#B3B3B3",
+          400: "#3E878F",
+          500: "#A0A0A0",
+          600: "#666666",
+          700: "#4D4D4D",
+          800: "#333333",
+          900: "#1A1A1A"
+        }
+      }
+    }
   },
-  plugins: [require("daisyui")],
+  plugins: [
+    plugin(function ({ addComponents }) {
+      addComponents({
+        ".btn-primary": {
+          backgroundColor: `${config.theme.extend.colors.primary["600"]} !important`,
+          color: "#fff !important",
+          borderColor: `${config.theme.extend.colors.primary["600"]} !important`,
+          "&:hover": {
+            backgroundColor: `${config.theme.extend.colors.primary["600"]} !important`
+          }
+        }
+      });
+    }),
+    require("daisyui")
+  ],
   daisyui: {
     themes: ["light"]
   }
 };
+
+module.exports = config;
