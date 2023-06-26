07b2b1736ea62772ee8cde85f09ea015dc8afed3
melih şahin <melih.sahin@missafir.com>
2023-06-26 12:53:51 +0300

    feat(MWSV-9): Add header section response for home page

--- src/api/link/controllers/link.js
+++ src/api/link/controllers/link.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * page controller
+ * link controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::page.page');
+module.exports = createCoreController('api::link.link');
--- src/api/link/routes/link.js
+++ src/api/link/routes/link.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * page router
+ * link router
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::page.page');
+module.exports = createCoreRouter('api::link.link');
