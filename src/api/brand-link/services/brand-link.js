e84eba72e2dcbde739a93f4533a267fce59842bc
melih şahin <melih.sahin@missafir.com>
2023-07-04 11:06:59 +0300

    MWSV-9 Update footer and footer brand

---
+++ src/api/brand-link/services/brand-link.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * brand-link service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::brand-link.brand-link');
