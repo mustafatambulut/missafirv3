297397be81e00356884bdc722398958893361dd0
mustafa batar <mustafa.batar@missafir.com>
2023-07-07 17:37:02 +0300

    feat(MWSV-43): created card component

---
+++ src/components/atoms/card/Card.tsx
@@ -0,0 +1,11 @@
+import {ICard} from "@/components/atoms/card/types";
+
+const Card = ({ children,...props }:ICard) => {
+  return (
+    <div className="card" {...props}>
+      <div className="card-body p-6 cursor-default">{children}</div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/components/atoms/card/types.ts
@@ -0,0 +1,6 @@
+import React from "react";
+
+export interface ICard {
+  children: React.ReactNode;
+  className?: string;
+}
