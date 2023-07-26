d9cb8e769b3b8594cb5b4d556eac2c8aa4b56ceb
melih şahin <melih.sahin@missafir.com>
2023-07-26 12:50:21 +0300

    feat(main): Update seed data

--- src/seed/entities/home.js
+++ src/seed/entities/home.js
@@ -14,6 +14,30 @@ module.exports = {
 
     await destroyAll(entity);
 
+    const partners = [];
+    const partnerImages = ["https://svgshare.com/i/vHQ.svg", "https://svgshare.com/i/vL1.svg", "https://svgshare.com/i/vHQ.svg"];
+
+    for (let i = 0; i < 20; i++) {
+      partners.push(
+        {
+          image: partnerImages.map((image) => image)[Math.floor(Math.random() * partnerImages.length)],
+          link: faker.internet.url(),
+          label: faker.word.verb()
+        }
+      );
+    }
+
+    const benefits = [];
+    for (let i = 0; i < 3; i++) {
+      benefits.push(
+        {
+          image: "https://svgshare.com/i/vHM.svg",
+          title: "In-house technology and world-class marketing",
+          description: faker.lorem.paragraph()
+        }
+      );
+    }
+
     strapi.entityService.create(entity, {
       data: {
         header: {
@@ -24,8 +48,8 @@ module.exports = {
           },
           buttons: [
             {
-              label: faker.word.verb(),
-              image: faker.image.avatar(),
+              label: "Become a homeowner",
+              image: "https://svgshare.com/i/vAP.svg",
               link: faker.internet.url(),
               publishedAt: new Date()
             }
@@ -43,8 +67,8 @@ module.exports = {
         body: [
           {
             __component: "sections.hero",
-            image: faker.image.avatar(),
-            title: faker.lorem.sentence()
+            image: "https://i.ibb.co/xq2vBny/hero.png",
+            title: "Home wherever you go"
           },
           {
             __component: "sections.benefit-guest",
@@ -56,7 +80,7 @@ module.exports = {
           {
             __component: "sections.cities",
             header: {
-              title: faker.lorem.sentence(),
+              title: "Best cities to discover",
               description: faker.lorem.paragraph()
             },
             cities: await strapi.db.query(cityEntity).findMany({})
@@ -67,23 +91,17 @@ module.exports = {
               title: faker.lorem.sentence(),
               description: faker.lorem.paragraph()
             },
-            body: [
-              {
-                image: faker.image.avatar(),
-                title: faker.lorem.sentence(),
-                description: faker.lorem.paragraph()
-              }
-            ]
+            body: benefits
           },
           {
             __component: "sections.premium",
             header: {
-              title: faker.lorem.sentence(),
+              title: "Hearbeats & Happenings We Got You",
               description: faker.lorem.paragraph()
             },
             body: {
               title: faker.lorem.sentence(),
-              image: faker.image.avatar()
+              image: "https://svgshare.com/i/voH.svg"
             },
             footer: {
               label: faker.word.verb(),
@@ -144,13 +162,23 @@ module.exports = {
           {
             __component: "sections.tech-experience",
             header: {
-              title: faker.lorem.sentence(),
+              title: "Streamlined MissafirTech Experience",
               description: faker.lorem.paragraph()
             },
             body: [
               {
                 title: faker.lorem.sentence(),
-                image: faker.image.avatar(),
+                image: "https://i.ibb.co/1MrZ78J/Group-114.png",
+                description: faker.lorem.paragraph()
+              },
+              {
+                title: faker.lorem.sentence(),
+                image: "https://i.ibb.co/1MrZ78J/Group-114.png",
+                description: faker.lorem.paragraph()
+              },
+              {
+                title: faker.lorem.sentence(),
+                image: "https://i.ibb.co/1MrZ78J/Group-114.png",
                 description: faker.lorem.paragraph()
               }
             ]
@@ -164,7 +192,23 @@ module.exports = {
             body: [
               {
                 header_label: faker.lorem.sentence(),
-                header_image: faker.image.avatar(),
+                header_image: "https://svgshare.com/i/vL0.svg",
+                info: faker.lorem.paragraph(),
+                title: faker.lorem.sentence(),
+                description: faker.lorem.paragraph(),
+                footer_desc: faker.lorem.paragraph(),
+              },
+              {
+                header_label: faker.lorem.sentence(),
+                header_image: "https://svgshare.com/i/vL0.svg",
+                info: faker.lorem.paragraph(),
+                title: faker.lorem.sentence(),
+                description: faker.lorem.paragraph(),
+                footer_desc: faker.lorem.paragraph(),
+              },
+              {
+                header_label: faker.lorem.sentence(),
+                header_image: "https://svgshare.com/i/vL0.svg",
                 info: faker.lorem.paragraph(),
                 title: faker.lorem.sentence(),
                 description: faker.lorem.paragraph(),
@@ -178,21 +222,15 @@ module.exports = {
               title: faker.lorem.sentence(),
               description: faker.lorem.paragraph()
             },
-            body: [
-              {
-                image: faker.image.avatar(),
-                link: faker.internet.url(),
-                label: faker.word.verb(),
-              }
-            ]
+            body: partners
           },
         ],
         footer: {
           header: {
-            image: faker.image.avatar(),
+            image: "https://www.missafir.com/wp-content/uploads/2022/12/logo-2.svg",
             description: faker.lorem.paragraph(),
-            buttonImage: faker.image.avatar(),
-            buttonLabel: faker.lorem.word(),
+            buttonImage: "https://svgshare.com/i/vAP.svg",
+            buttonLabel: "Become a homeowner",
             buttonLink: faker.internet.url()
           },
           body: [
