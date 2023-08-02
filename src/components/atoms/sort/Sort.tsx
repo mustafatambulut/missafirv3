f82cd8c67178fa5cd4e3e8c95da34bccf0d85ea4
mustafa batar <mustafa.batar@missafir.com>
2023-08-02 16:43:57 +0300

    feature(MWSV-76): created Sort component.

---
+++ src/components/atoms/sort/Sort.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Sort = () => {
+  return (
+    <div className="grid grid-rows-1 gap-3 ">
+      <div className="text-gray-500 text-base font-mi-sans-semi-bold">
+        Artan Fiyat
+      </div>
+      <div className="text-gray-500 text-base font-mi-sans-semi-bold">
+        Azalan Fiyat
+      </div>
+      <div className="text-gray-500 text-base font-mi-sans-semi-bold">
+        Yeniden Eskiye
+      </div>
+      <div className="text-gray-500 text-base font-mi-sans-semi-bold">
+        Eskiden Yeniye
+      </div>
+      <div className="text-gray-500 text-base font-mi-sans-semi-bold">
+        Çok Değerlendirilenler
+      </div>
+      <div className="text-gray-500 text-base font-mi-sans-semi-bold">
+        Yeni Eklenenler
+      </div>
+    </div>
+  );
+};
+
+export default Sort;
