537c25740d4e0bb6f81e70698a1c1acbfc7cef68
melih şahin <melih.sahin@missafir.com>
2023-08-09 14:35:13 +0300

    feat(MWSV-116): Refactor BenefitOwner component

--- src/components/atoms/benefitOwner/BenefitOwner.tsx
+++ src/components/atoms/benefitOwner/BenefitOwner.tsx
@@ -1,63 +1,51 @@
 "use client";
-import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import { filter, first, get, map } from "lodash";
+import { get, map } from "lodash";
 
-import { BODY, HOME } from "@/app/constants";
-import { getPageDataByComponent } from "@/utils/helper";
+import useFetchData from "@/app/hooks/useFetchData";
+import { IBenefitOwner } from "@/components/atoms/benefitOwner/types";
+import { BODY } from "@/app/constants";
+import { BENEFIT_OWNER_SECTION } from "@/components/atoms/benefitOwner/constants";
 
 import Card from "@/components/atoms/card/Card";
 import Section from "@/components/molecules/section/Section";
+import Loading from "@/components/atoms/loading/Loading";
 
 const BenefitOwner = () => {
-  const [benefit, setBenefit] = useState(null);
-
-  const fetchData = async () => {
-    const response = await getPageDataByComponent(HOME, BODY);
-    const benefitData = first(
-      filter(
-        response,
-        (item) => item["__component"] === "sections.benefit-owner"
-      )
-    );
-    setBenefit(benefitData);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
+  const benefit = useFetchData<IBenefitOwner>(BODY, BENEFIT_OWNER_SECTION);
 
   return (
-    <>
-      {benefit && (
-        <Section
-          className="px-4 lg:px-8 mt-14"
-          title={get(benefit, "header.title")}
-          description={get(benefit, "header.description")}>
-          <div className="grid grid-cols-1 lg:grid-cols-3 lg:gap-x-8 lg:my-20">
-            {map(get(benefit, "body"), (benefit, key) => (
-              <Card key={key} className="mb-12 lg:mb-0">
-                <div className="flex flex-col items-center">
+    <Loading isLoading={!benefit} loader={<p>Loading feed...</p>}>
+      {/*todo: skeleton eklenecek*/}
+      <Section
+        className="px-4 lg:px-8 mt-14"
+        title={get(benefit, "header.title")}
+        description={get(benefit, "header.description")}>
+        <div className="grid grid-cols-1 lg:grid-cols-3 lg:gap-x-8 lg:my-20">
+          {map(get(benefit, "body"), (benefit, key) => (
+            <Card key={key} className="mb-12 lg:mb-0">
+              <div className="flex flex-col items-center">
+                {get(benefit, "image") && (
                   <Image
-                    className="mb-7"
-                    src={get(benefit, "image") || ""}
                     alt="image"
                     width={64}
                     height={64}
+                    className="mb-7"
+                    src={get(benefit, "image")}
                   />
-                  <h3 className="text-center mb-3 text-28 font-mi-sans-semi-bold text-gray-800">
-                    {get(benefit, "title")}
-                  </h3>
-                  <div className="text-center text-gray-600 tex-2xl line-clamp-3">
-                    {get(benefit, "description")}
-                  </div>
+                )}
+                <h3 className="text-center mb-3 text-28 font-mi-sans-semi-bold text-gray-800">
+                  {get(benefit, "title")}
+                </h3>
+                <div className="text-center text-gray-600 tex-2xl line-clamp-3">
+                  {get(benefit, "description")}
                 </div>
-              </Card>
-            ))}
-          </div>
-        </Section>
-      )}
-    </>
+              </div>
+            </Card>
+          ))}
+        </div>
+      </Section>
+    </Loading>
   );
 };
 
