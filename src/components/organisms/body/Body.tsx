533cb8ad3c54a3a605c5f58087d661765dcdf99e
melih şahin <melih.sahin@missafir.com>
2023-08-09 14:42:47 +0300

    feat(MWSV-116): Refactor News component

--- src/components/organisms/body/Body.tsx
+++ src/components/organisms/body/Body.tsx
@@ -1,27 +1,27 @@
 import Premium from "@/components/atoms/premium/Premium";
 import InfoSection from "@/components/atoms/infoSection/InfoSection";
 import BenefitOwner from "@/components/atoms/benefitOwner/BenefitOwner";
-import TechExperience from "@/components/molecules/techExperience/TechExperience";
 import News from "@/components/molecules/news/News";
 import Hero from "@/components/molecules/hero/Hero";
 import Cities from "@/components/molecules/cities/Cities";
-import Partners from "@/components/molecules/partners/Partners";
-import Testimonials from "@/components/molecules/testimonials/Testimonials";
+import Partner from "@/components/molecules/partner/Partner";
+import Testimonial from "@/components/molecules/testimonial/Testimonial";
 import ListingDetails from "@/components/molecules/listingDetail/ListingDetails";
+import TechExperience from "@/components/molecules/techExperience/TechExperience";
 
 const Body = () => {
   return (
     <div>
       <Hero />
       <Cities />
-      <ListingDetails />
+      <ListingDetails /> {/*todo:refactor edilecek*/}
       <Premium />
       <TechExperience />
-      <Testimonials />
-      <Partners />
+      <Testimonial />
+      <Partner />
       <BenefitOwner />
       <News />
-      <InfoSection collapsable={true} />
+      <InfoSection collapsable={true} /> {/*todo:refactor edilecek*/}
     </div>
   );
 };
