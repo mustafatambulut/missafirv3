c0656c8c7a9c7ac58322ce7f29ff9d082b162a01
melih şahin <melih.sahin@missafir.com>
2023-08-12 20:58:32 +0300

    feat(MWSV-87): Update landingSlice

--- src/redux/features/landingSlice/landingSlice.ts
+++ src/redux/features/landingSlice/landingSlice.ts
@@ -2,6 +2,7 @@ import { get } from "lodash";
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 import { getPage } from "@/service/api";
+import { ILandingState } from "@/redux/features/landingSlice/types";
 
 export const fetchDataByPage = createAsyncThunk(
   "landing/fetchDataByPage",
@@ -11,27 +12,22 @@ export const fetchDataByPage = createAsyncThunk(
   }
 );
 
-interface LandingState {
-  entities: [];
-  loading: boolean;
-}
-
 const initialState = {
   entities: [],
   loading: true
-} as LandingState;
+} as ILandingState;
 
 const landingSlice = createSlice({
   name: "landing",
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(fetchDataByPage.pending, (state: LandingState): void => {
+    builder.addCase(fetchDataByPage.pending, (state: ILandingState): void => {
       state.loading = true;
     });
     builder.addCase(
       fetchDataByPage.fulfilled,
-      (state: LandingState, action) => {
+      (state: ILandingState, action) => {
         get(state, "entities").push(get(action, "payload"));
         state.loading = false;
       }
