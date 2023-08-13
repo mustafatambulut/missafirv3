9bcd042eeb9bd31361802bb34ec2124b123f620e
melih şahin <melih.sahin@missafir.com>
2023-08-13 22:21:16 +0300

    feat(MWSV-87): Add Alert component

---
+++ src/components/atoms/alert/Alert.tsx
@@ -0,0 +1,34 @@
+import classNames from "classnames";
+import { IAlert } from "@/components/atoms/alert/types";
+
+const Alert = ({
+  icon,
+  title,
+  onClick,
+  className = "",
+  variant = "success"
+}: IAlert) => {
+  const containerClass = classNames("", {
+    "bg-blue-100": variant === "info",
+    "bg-primary-100": variant === "danger",
+    "bg-green-100": variant === "success",
+    "bg-yellow-100": variant === "warning"
+  });
+
+  const textClass = classNames("", {
+    "text-blue-600": variant === "info",
+    "text-primary-600": variant === "danger",
+    "text-green-600": variant === "success",
+    "text-yellow-600": variant === "warning"
+  });
+
+  return (
+    <div
+      className={`flex rounded-lg items-center justify-between p-3 my-2 ${containerClass} ${className}`}>
+      <p className={textClass}>{title}</p>
+      <span onClick={onClick}>{icon}</span>
+    </div>
+  );
+};
+
+export default Alert;
