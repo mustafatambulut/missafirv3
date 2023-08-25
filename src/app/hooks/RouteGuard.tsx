5b601746eb0db6e5285d24a4e73c57fdc5b65d0f
melih şahin <melih.sahin@missafir.com>
2023-08-25 12:17:02 +0300

    fix(MVC-38): RouteGuard for ssr

--- src/app/hooks/RouteGuard.tsx
+++ src/app/hooks/RouteGuard.tsx
@@ -1,39 +1,31 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { includes, split } from "lodash";
 import { usePathname, useRouter } from "next/navigation";
 
-import { LOCALES, PROTECTED } from "@/app/constants";
 import {
   checkSameItem,
   getCurrentLang,
   getSessionStorage
 } from "@/utils/helper";
+import { LOCALES, PROTECTED } from "@/app/constants";
 
 const RouteGuard = ({ children }) => {
   const router = useRouter();
   const pathname = usePathname();
-  const [authorized, setAuthorized] = useState(false);
 
   const authCheck = () => {
     const lang = getCurrentLang();
-
     if (!getSessionStorage("token")) {
       const isProtected = checkSameItem(PROTECTED, split(pathname, "/"));
 
       if (isProtected) {
-        setAuthorized(false);
         const hasLang = checkSameItem(split(pathname, "/"), LOCALES);
         router.push(`${!hasLang ? lang : ""}/login`);
         return;
       }
-      setAuthorized(true);
-    }
-
-    if (getSessionStorage("token")) {
-      includes(split(pathname, "/"), "login")
-        ? router.push("/")
-        : setAuthorized(true);
+    } else {
+      if (includes(split(pathname, "/"), "login")) router.push("/");
     }
   };
 
@@ -41,9 +33,7 @@ const RouteGuard = ({ children }) => {
     authCheck();
   }, [getSessionStorage("token")]);
 
-  return includes(split(pathname, "/"), "login")
-    ? children
-    : authorized && children;
+  return children;
 };
 
 export default RouteGuard;
